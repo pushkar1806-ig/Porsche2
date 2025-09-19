9d4cfda42d66d5884f41fb8f6029b27a6ec05cb5
pushkar1806-ig <pushkarbhangale08@gmail.com>
2025-09-19 18:28:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleSidebar() {
+  document.getElementById("sidebar").classList.toggle("active");
+}
+function toggleNotification(){
+  document.getElementById('noti-bar').classList.toggle('active');
+}
